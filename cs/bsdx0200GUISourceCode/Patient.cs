e1fc2de4ba878069490e30319c085b171664ee2c
shabiel <sam.habiel@gmail.com>
2011-03-20 07:22:52 +0000

    Patient class added. Simple class to represent guess what.

---
+++ cs/bsdx0200GUISourceCode/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndianHealthService.ClinicalScheduling
+{
+    /// <summary>
+    /// Puppet standing for a Real Patient
+    /// </summary>
+    public class Patient
+    {
+        public int DFN { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string ID { get; set; }
+        public string HRN { get; set; }
+        public List<CGAppointment> Appointments { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string HomePhone { get; set; }
+        public string WorkPHone { get; set; }
+        public string CellPhone { get; set; }
+    }
+}
